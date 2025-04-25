d4a8e499f80288be8c3e2d94f8123cd04024f847
IsaacK47 <isaackayombo41@gmail.com>
2025-04-25 21:06:51 +0200

    Creating form section

---
+++ components/Meme.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Meme() {
+    return (
+        <main>
+            <form>
+                <input type="text"></input>
+                <input type="text"></input>
+                <button>Get a new meme image 🆕</button>
+            </form>
+        </main>
+    )
+}
