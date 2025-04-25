85e9ccc12d2c812681f885b2423b971d18bcc5a8
IsaacK47 <isaackayombo41@gmail.com>
2025-04-25 22:31:58 +0200

    Setting get meme image function

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,12 +1,17 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+    function getMemeImage() {
+        
+    }
+
     return (
         <main>
             <div className="form">
                 <input type="text" className="form--input" placeholder="Top text"></input>            
                 <input type="text" className="form--input" placeholder="Bottom text"></input>
-                <button className="form--button">Get a new meme image 🆕</button>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image 🆕</button>
             </div>
         </main>
     )
