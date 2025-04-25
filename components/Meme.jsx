aad1da7d716f2fe1a1a62fc0f2a1f279836a0adb
IsaacK47 <isaackayombo41@gmail.com>
2025-04-25 21:36:05 +0200

    Styling Meme conponent form

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -3,10 +3,10 @@ import React from "react"
 export default function Meme() {
     return (
         <main>
-            <form>
-                <input type="text"></input>
-                <input type="text"></input>
-                <button>Get a new meme image 🆕</button>
+            <form className="form">
+                <input type="text" className="form--input" placeholder="Top text"></input>            
+                <input type="text" className="form--input" placeholder="Bottom text"></input>
+                <button className="form--button">Get a new meme image 🆕</button>
             </form>
         </main>
     )
