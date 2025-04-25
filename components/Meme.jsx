2fb1873a42cabc039eea66eb766b86bdfa16d7c4
IsaacK47 <isaackayombo41@gmail.com>
2025-04-25 21:54:59 +0200

    Getting meme data

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -3,11 +3,11 @@ import React from "react"
 export default function Meme() {
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top text"></input>            
                 <input type="text" className="form--input" placeholder="Bottom text"></input>
                 <button className="form--button">Get a new meme image 🆕</button>
-            </form>
+            </div>
         </main>
     )
 }
