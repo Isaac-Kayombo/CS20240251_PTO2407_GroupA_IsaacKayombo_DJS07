9040e890fa07b6c0dfe61fc5b746e79ab888fb9b
IsaacK47 <isaackayombo41@gmail.com>
2025-04-26 09:21:49 +0200

    Refactoring state management logic for meme component

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -2,11 +2,22 @@ import React from "react"
 import memesData from "../memesData"
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState("")
-    function getMemeImage() {
-        const memesArray = memesData.data.memes
+    //const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+        function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(preMeme => ({
+            ...preMeme,
+            randomImage: url
+        }))
     }
 
     return (
@@ -16,7 +27,7 @@ export default function Meme() {
                 <input type="text" className="form--input" placeholder="Bottom text"></input>
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image 🆕</button>
             </div>
-            <img src={memeImage} className="meme--image"/>
+            <img src={meme.randomImage} className="meme--image"/>
         </main>
     )
 }
